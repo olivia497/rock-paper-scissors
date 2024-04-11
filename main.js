95f38937381ff397bd12171830b1427dcf0612fb
Olivia <oliviafedato@gmail.com>
2024-04-10 18:16:47 -0600

    function to check the win of player vs. bot added

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ scissors.addEventListener('click', () => {
   playerChoice = 'scissors'
   checkWin()
 });
+
+//Check the win of player v bot
+function checkWin(){
+  let botChoice = rockPaperScissorsCheck();
+
+  document.querySelector('.playerChoice').innerText = playerChoice
+  document.querySelector('.botChoice').innerText = botChoice
+
+  if( (playerChoice === 'rock' && botChoice === 'scissors') || (playerChoice === 'paper' && botChoice === 'rock') || (playerChoice === 'scissors' && botChoice === 'paper') ){
+    document.querySelector('.winLoseTie').innerText = 'You win'
+  }else if(playerChoice === botChoice){
+    document.querySelector('.winLoseTie').innerText = 'You tied'
+  }else{
+    document.querySelector('.winLoseTie').innerText = 'You lose'
+  };
+};
