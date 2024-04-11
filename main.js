2a1dee1d2d85b9d62a08a7407e95e9f11262f617
Olivia <oliviafedato@gmail.com>
2024-04-10 18:15:57 -0600

    get player choice added

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+
+//Get player choice 
+rock.addEventListener('click', () => {
+  playerChoice = 'rock'
+  checkWin()
+});
+paper.addEventListener('click', () => {
+  playerChoice = 'paper'
+  checkWin()
+});
+scissors.addEventListener('click', () => {
+  playerChoice = 'scissors'
+  checkWin()
+});
